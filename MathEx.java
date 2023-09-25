3e75abf280a5797e2ff9ac35ed1a8fd899d4bd26
Shivaboyz <meiyurshiv11@gmail.com>
2023-09-24 21:28:20 -0400

    hi

--- MathEx.java
+++ MathEx.java
@@ -2,20 +2,44 @@ public class MathEx{
 
     public static void main(String[] args){
 
-        int last= swap(91);
-        System.out.println(last);
 
+        System.out.println(swap(49));
+        System.out.println(swap(298));
+        System.out.println(swap(17359));
+        System.out.println(swap(1762));
+
+        timeLeft(2,56,7,20);
+        timeLeft(5,26,9,29);
+        timeLeft(1,40,3,19);
+
+        System.out.println(dayOfWeek(6,12));
+        System.out.println(dayOfWeek(0,17));
+        System.out.println(dayOfWeek(3,25));
 
 
     }
 
     public static int swap(int initial){
 
-        int intialhundreds= initial/100; 
-        int initialtens= initial/10;
-        int initalones= initial%10;
-        int last= initial*100 + initalones*10 + initialtens;
-        return last;
+        int numbrrr= initial - (10*((initial%100)/10)) - (initial%10) + ((10*(initial%10)) + ((initial%100)/10)); 
+        return numbrrr; 
+
     }
     
+
+    public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
+
+        int totalTime = ((depHour*60) + depMin) - ((curHour*60) + curMin);
+        System.out.println(totalTime/60 + " hours and " + totalTime%60 + " minutes.");
+
+    }
+
+
+    public static int dayOfWeek(int JanUno, int datee){
+
+       int doww = ((JanUno - 1) + datee)%7;
+       return doww;  
+
+    }
+
 }

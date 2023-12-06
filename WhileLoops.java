32f5eeec72a7c1464d9a0edcb51fe3a219b91304
Shivaboyz <meiyurshiv11@gmail.com>
2023-12-06 14:00:30 -0500

    top opp

--- WhileLoops.java
+++ WhileLoops.java
@@ -13,15 +13,56 @@ public class WhileLoops {
     public int sumDigits(int num) {
         int tensnum = 10;
         int sum = 0;
-        while ((num/tensnum) > 0) {
-            sum += ((num%tensnum)/(tensnum/10));
+        int adder = 0;
+        while (((10*num)/tensnum) > 0) {
+            adder = (num%tensnum);
+            adder /= (tensnum/10);
+            sum += adder;
             tensnum *= 10;
         }
         return sum;
     }
 
+    public int howManyYears(double startpop, double endpop) {
+        startpop *= 1000000;
+        endpop *= 1000000;
+        int yearcount = 0;
+        while (startpop >= endpop) {
+            startpop *= 1.0113;
+            yearcount ++;
+        }
+        return yearcount;
+    }
 
+    public void printSum(int num) {
+        int queue = 1;
+        int sum = 0;
+        while (queue != num) {
+            sum += queue;
+            System.out.print(queue + " + ");
+            queue ++;
+        }
+        System.out.println(" = " + (sum+num));
+    }
 
+    public boolean isPerfectSquare(int num) {
+        int queuenum = 1;
+        int adder = 1;
+        int sum = 0;
+        while (queuenum != num){
+            sum += adder;
+            adder += 2;
+            queuenum ++;
+        }
+
+        if (queuenum == num) {
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -36,8 +77,14 @@ public class WhileLoops {
         System.out.println(loooops.sumDigits(10298));
         System.out.println(loooops.sumDigits(198));
         System.out.println("howManyYears method");
-
-
-
+        System.out.println(loooops.howManyYears(120,150));
+        System.out.println(loooops.howManyYears(169,256));
+        System.out.println(loooops.howManyYears(776,890));
+        System.out.println("printSum method");
+        loooops.printSum(36);
+        loooops.printSum(79);
+        loooops.printSum(99);
+        System.out.println("isPerfectSquare method");
+        
     }
 }

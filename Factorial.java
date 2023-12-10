67edae5db028e6b612db950e212d83bdbe9de188
Shivaboyz M <meiyurshiv11@gmail.com>
2023-12-10 16:23:34 +0000

    temple

--- Factorial.java
+++ Factorial.java
@@ -6,7 +6,33 @@ public class Factorial {
         return sum;
     }
 
+    public double calcE () {
+        double oldE = 0;
+        double newE = 1;
+        int count = 1;
 
+        while ((newE - oldE) > 0.001) {
+            oldE = newE;
+            newE += (1.0/calcFactorial(count));
+            count++;
+        }
+
+        return newE;
+    }
+
+    public double calcEX (int num) {
+        double oldE = 0;
+        double newE = 1;
+        int count = 1;
+
+        while ((newE - oldE) > 0.001) {
+            oldE = newE;
+            newE += ((Math.pow(num, count))/(calcFactorial(count)));
+            count++;
+        }
+
+        return newE;
+    }
 
 
 
@@ -15,6 +41,11 @@ public class Factorial {
         System.out.println("calcFactorial method");
         for(int j = 1; j < 21; j++){
             System.out.println(ft.calcFactorial(j));
+        }
+        System.out.printf("\ne is %2.3f \n \n", ft.calcE());
+
+        for(int i = 1; i <= 5; i++) {
+            System.out.printf("e to the power of %d = %2.3f \n", i, ft.calcEX(i));
         }
 
     }

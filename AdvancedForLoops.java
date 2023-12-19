e18dba125dbdce0725680944f4ece49bb47f3b6a
Shivaboyz M <meiyurshiv11@gmail.com>
2023-12-19 18:14:24 +0000

    micro

--- AdvancedForLoops.java
+++ AdvancedForLoops.java
@@ -24,11 +24,11 @@ public class AdvancedForLoops {
 
     public void alternate(int n) {
 
-        for (int g = n; g>0; g--) {
+        for (int g = 0; g<=n; g++) {
 
             if ((g%2) == 0) {
 
-        for (int i = n; i>0; i--) {
+        for (int i = g; i>0; i--) {
             System.out.print(i);
         }
         System.out.println();
@@ -36,7 +36,7 @@ public class AdvancedForLoops {
 
             else {
 
-        for (int j = 1; j<=n; j++) {
+        for (int j = 1; j<=g; j++) {
             System.out.print(j);
         }
         System.out.println();
@@ -45,6 +45,26 @@ public class AdvancedForLoops {
     }
 }
 
+    public void isosceles(int n) {
+        for (int m = 1; m<=n; m++) {
+        for (int i = (n-m); i>1; i--) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j<= (m-1); j++) {
+            System.out.print(j);
+        }
+        for (int v = m; v>(m-1); v--) {
+            System.out.print(v);
+        }
+        for(int z = (m-1); z>=1; z--) {
+            System.out.print(z);
+        }
+        for (int k = (n-m); k>1; k--) {
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+    }
 
 public static void main(String[]args) {
     AdvancedForLoops afl = new AdvancedForLoops();
@@ -53,6 +73,12 @@ public static void main(String[]args) {
     System.out.println("alternate method");
     afl.alternate(6);
     afl.alternate(5);
+    afl.alternate(3);
+    afl.alternate(9);
+    System.out.println("isosceles method");
+    afl.isosceles(7);
+    afl.isosceles(5);
+    afl.isosceles(9);
 
 }
 

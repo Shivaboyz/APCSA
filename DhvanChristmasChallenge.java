016044a431697e59e8da59adae60e88f0bfa55dc
Shivaboyz <meiyurshiv11@gmail.com>
2023-12-20 13:59:48 -0500

    flask

---
+++ DhvanChristmasChallenge.java
@@ -0,0 +1,34 @@
+public class DhvanChristmasChallenge {
+
+    public void ErlenmeyerFlask(int BotHeight, int MouthWidth, int MouthHeight) {
+            
+        for(int g = BotHeight; g>0; g--) {
+            System.out.print(" ");
+        }
+        for (int i = MouthWidth; i>0; i--) {
+                System.out.print("_");
+            }
+            System.out.println();
+        for (int j = (MouthHeight-1); j>0; j--) {
+            System.out.print("|");
+            for (int k = (MouthWidth-2); k>0; k--) {
+                    System.out.print(" ");
+                }
+            System.out.println("|");
+            }
+        for (int r = 0; r<(BotHeight-1); r++) {
+            System.out.print("/");
+            for (int w = MouthWidth+(2*r); w>0; w--) {
+                System.out.print(" ");
+                }
+            System.out.println("\");
+            }
+            
+        for (int q = ())    
+
+            
+        }
+
+    }
+
+}

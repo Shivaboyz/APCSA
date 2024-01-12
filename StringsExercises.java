29453e54d1103b561813c5d2846561c2745aafc7
Shivaboyz <meiyurshiv11@gmail.com>
2024-01-12 14:00:17 -0500

    bomboclat

--- StringsExercises.java
+++ StringsExercises.java
@@ -7,29 +7,82 @@ public class StringsExercises {
     }
 
     public String convertName(String name) {
-        String trimmed = new String();
-        String fin = new String();
-        trimmed = name.trim();
-        fin += (trimmed.substring(0, trimmed.indexOf(",")));
-        int spaces = trimmed.indexOf(" ");
-        if (spaces == -1) {
-            fin += trimmed.substring(1+(trimmed.indexOf(",")));
-        }
+        String ln = new String();
+        String fn = new String();
+        ln += name.substring(0, name.indexOf(","));
+        fn += name.substring(1+name.indexOf(","));
+        fn = fn.trim();
+        ln = ln.trim();
+        return (fn + " " + ln);
+    }
 
-        else {
-            fin += trimmed.substring((spaces+1));
-        }
+    public String negative(String num) {
+        String fin = new String();
+        fin += num; 
+        fin = fin.replace("1","2");
+        fin = fin.replace("0", "1");
+        fin = fin.replace("2", "0");
         return fin; 
+    }
+
+    public String dateString(String dateStr) {
+        String mon = dateStr.substring(0,2);
+        String day = dateStr.substring(3,5);
+        String year = dateStr.substring(6);
+        return (mon + "-" + day + "-" + year);
+    }
+
+    public String dateString2(String dateStr) {
+        String fin = new String();
+        int firstSpace = dateStr.indexOf("/");
+        int secondSpace = dateStr.indexOf("/", firstSpace);
+        fin += ((dateStr.substring(0,firstSpace)) + "-" + (dateStr.substring()))
     }
 
     public static void main(String[] args) {
         StringsExercises se = new StringsExercises();
+
         System.out.println(se.scroll("Hello World"));
         System.out.println(se.scroll("happy"));
         System.out.println(se.scroll("h"));
-        System.out.println(se.convertName(" Reubenstein, Lori Renee "));
+    
+        System.out.println(se.convertName(" Reubenstein, Lori Renee  	"));
         System.out.println(se.convertName("Biden,Joe"));
         System.out.println(se.convertName("the Clown, Bozo"));
+    
+        System.out.println(se.negative("0010111001"));
+        System.out.println(se.negative("11111111"));
+    
+        System.out.println("04/20/2014 becomes " + se.dateString("04/20/2014"));
+        //System.out.println("04/20/2014 becomes " + se.dateString2("04/20/2014"));
+        //System.out.println("4/20/2014 becomes " + se.dateString2("4/20/2014"));
+        //System.out.println("04/2/2014 becomes " + se.dateString2("04/2/2014"));
+        //System.out.println("4/2/2024 becomes " + se.dateString2("4/2/2024"));
+    
+        //System.out.println("\nstartsWith");
+        //System.out.println(se.startsWith("architecture", "arch"));
+        //System.out.println(se.startsWith("architecture", "a"));
+        //System.out.println(se.startsWith("arch", "architecture"));
+        //System.out.println(se.startsWith("architecture", "rch"));
+        //System.out.println(se.startsWith("architecture", "architecture"));
+    
+        //System.out.println("\nendsWith");
+        //System.out.println(se.endsWith("astronomy", "nomy"));
+        //System.out.println(se.endsWith("astronomy", "y"));
+        //System.out.println(se.endsWith("astronomy", "nom"));
+        //System.out.println(se.endsWith("nomy", "astronomy"));
+        //System.out.println(se.endsWith("astronomy", "astronomy"));
+    
+        //System.out.println("\nremoveTag");
+        //System.out.println(se.removeTag("<b>Hello World</b>", "b"));
+        //System.out.println(se.removeTag("<b>Hello World</b>", "head"));
+        //System.out.println(se.removeTag("Hello World</b>", "b"));
+        //System.out.println(se.removeTag("<b>Hello World", "b"));
+        //System.out.println(se.removeTag("</img>Hello World<img>", "img"));
+        //System.out.println(se.removeTag("Happy Birthday <b>Hello World</b>", "b"));
+        //System.out.println(se.removeTag("<title>Hello World</title> Happy Birthday", "title"));
+        //System.out.println(se.removeTag("Happy <b>Hello World</b> Birthday", "b"));
+        
     }
 
 }

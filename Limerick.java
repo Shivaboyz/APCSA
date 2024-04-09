124b16cf4dd1ffe94972fcd8831c3c9b93fc442f
Shivaboyz <meiyurshiv11@gmail.com>
2024-04-09 13:51:58 -0400

    peoms

--- Limerick.java
+++ Limerick.java
@@ -2,4 +2,15 @@ public class Limerick extends Poem {
     public Limerick() {
         super(5);
     }
+
+    public int getSyllables(int k) {
+        if((k == 3) || (k == 4)) return 6;
+        else return 9;
+    }
+
+    public void printRhythm() {
+        for(int i = 1; i<= getNumlines(); i++) {
+            funRhythm(getSyllables(i));
+        }
+    }
 }
--- Poem.java
+++ Poem.java
@@ -1,20 +1,27 @@
 public class Poem {
-    private int Liness; 
+    private int liness; 
 
     public Poem(){
-        Liness = 0;
+        liness = 0;
     }
 
     public Poem(int num) {
-        this.Liness = num;
+        this.liness = num;
     }
 
-    public int getNumLines(){
-        return Liness;
+    public int getNumlines(){
+        return liness;
     }
 
     public void printRhythm() {
         System.out.println("Free Verse!");
+    }
+
+    public void funRhythm(int num) {
+        for(int i = 1; i<=num; i++) {
+            if(i == num) System.out.println("ta");
+            else System.out.print("ta-");
+        }
     }
 }
 
